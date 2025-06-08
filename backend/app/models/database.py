9eb62c4f6f9866a27dfce0a0bc943e6bdb07a396
Rian Oliveira <>
2025-06-08 17:25:08 -0300

    Add .gitignore

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "sqlite:///./app/db.sqlite3"
+DATABASE_URL = "sqlite:///./db.sqlite3"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
