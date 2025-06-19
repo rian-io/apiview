3a13fbbc2de53a09fafeed3fbda36e897361995f
Rian Oliveira <>
2025-06-19 14:36:05 -0300

    Separate upload endpoint

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,45 +1,14 @@
 from fastapi import APIRouter
-from fastapi import UploadFile
-from fastapi import File
-from fastapi import HTTPException
 
 from app.api import health
-from app.models.database import APIDocument
-from app.models.database import SessionLocal
+from app.api import openapi_upload
 
-import shutil
-import uuid
 
 v1_prefix = "/api/v1"
 
+
 router = APIRouter()
 
+
 router.include_router(health.router, prefix=v1_prefix)
-
-
-@router.post("/api/v1/upload")
-async def upload_api_doc(file: UploadFile = File(...)):
-    ext = file.filename.split(".")[-1].lower()
-    if ext not in ["json", "yaml", "yml"]:
-        raise HTTPException(
-            status_code=400, detail="Only JSON or YAML files are accepted."
-        )
-
-    slug = str(uuid.uuid4())[:8]
-    save_path = f"app/uploads/{slug}.{ext}"
-
-    with open(save_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    db = SessionLocal()
-    doc = APIDocument(
-        filename=file.filename,
-        slug=slug,
-        filetype=ext,
-    )
-    db.add(doc)
-    db.commit()
-    db.refresh(doc)
-    db.close()
-
-    return {"slug": slug, "url": f"api/uploads/{slug}.{ext}"}
+router.include_router(openapi_upload.router, prefix=v1_prefix)
